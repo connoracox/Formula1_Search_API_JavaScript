05ecc098c7205fc58daf4fdc91ff064d949e88e2
Connor Cox <connor.a.cox@gmail.com>
2023-03-09 10:12:54 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const resultContainer = document.getElementById('table-results')
+
+async function getRacer(season, round) {
+
+    const response = await fetch(`http://ergast.com/api/f1/${season}/${round}/driverstandings.json`)
+    const data = await response.json()
+    
+    console.log(data)
+
+    for (let i = 0; i < 7; i++) {
+        resultContainer.innerHTML += `
+    
+        <table class="table table-dark">
+            <tr class="driverRow">
+                <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].position}</td>
+                <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.givenName} ${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.familyName}</td>
+                <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].constructorId}</td>
+                <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality}</td>
+                <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points}</td>
+            </tr> 
+        </table>
+        `  
+    }
+
+}
+   
+const formulaSearchForm = document.getElementById('formulaOneSearch')
+
+formulaSearchForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const idInputOne = formulaSearchForm.querySelector('#seasonId')
+    const idInputTwo = formulaSearchForm.querySelector('#roundId')
+    getRacer(idInputOne.value, idInputTwo.value)
+    idInputOne.value = ''
+    idInputTwo.value = ''
+    
+
+})
+
+const reloadform = document.querySelector('#reload')
+reloadform.addEventListener('click', () => {
+    window.location.reload(true);
+})
