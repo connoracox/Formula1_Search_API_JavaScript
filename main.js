962265e7f0991dbca833da1d0aa4128ea020bf57
Connor Cox <connor.a.cox@gmail.com>
2023-03-09 10:21:31 -0500

    added table headings

--- main.js
+++ main.js
@@ -4,13 +4,23 @@ async function getRacer(season, round) {
 
     const response = await fetch(`http://ergast.com/api/f1/${season}/${round}/driverstandings.json`)
     const data = await response.json()
-    
+
     console.log(data)
 
     for (let i = 0; i < 7; i++) {
         resultContainer.innerHTML += `
     
         <table class="table table-dark">
+        <thead>
+        <tr>
+          <th scope="col">Position</th>
+          <th scope="col">Name</th>
+          <th scope="col">Nationality</th>
+          <th scope="col">Sponsor</th>
+          <th scope="col">Points</th>
+        </tr>
+      </thead>
+      <tbody>
             <tr class="driverRow">
                 <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].position}</td>
                 <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.givenName} ${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.familyName}</td>
@@ -18,12 +28,13 @@ async function getRacer(season, round) {
                 <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality}</td>
                 <td>${data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points}</td>
             </tr> 
+        </tbody>
         </table>
-        `  
+        `
     }
 
 }
-   
+
 const formulaSearchForm = document.getElementById('formulaOneSearch')
 
 formulaSearchForm.addEventListener('submit', function (e) {
@@ -33,7 +44,7 @@ formulaSearchForm.addEventListener('submit', function (e) {
     getRacer(idInputOne.value, idInputTwo.value)
     idInputOne.value = ''
     idInputTwo.value = ''
-    
+
 
 })
 
